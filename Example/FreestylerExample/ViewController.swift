1a0a13c3f790785978056fbe608acbbcb796e373
Dávid Széles-Szép <david.szeles.szep@gmail.com>
2020-08-31 15:48:36 +0200

    Basic structure of sources, example project and workspace

---
+++ Example/FreestylerExample/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  FreestylerExample
+//
+//  Created by Dávid Szép on 2020. 08. 31..
+//  Copyright © 2020. W.UP. All rights reserved.
+//
+
+import UIKit
+import Freestyler
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
