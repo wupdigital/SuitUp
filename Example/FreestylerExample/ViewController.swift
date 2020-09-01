c13769b18a96380d134e2044231f191b6e4f1d45
Dávid Széles-Szép <david.szeles.szep@gmail.com>
2020-09-01 10:40:55 +0200

    Add change theme button to example with an action to change theme

--- Example/FreestylerExample/ViewController.swift
+++ Example/FreestylerExample/ViewController.swift
@@ -15,14 +15,52 @@ final class ViewController: UIViewController {
     @IBOutlet private weak var primaryView: UIView!
     @IBOutlet private weak var secondaryView: UIView!
     @IBOutlet private weak var tertiaryView: UIView!
+    @IBOutlet private weak var changeThemeButton: UIButton!
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        refreshStyles()
+        refreshChangeThemeButtonTitle()
+    }
+    
+    @IBAction private func changeTheme() {
+        switch Freestyler.shared.currentTheme?.identifier {
+        case ThemeIdentifier.sunrise.rawValue:
+            Freestyler.shared.changeTheme(to: ThemeIdentifier.sunset.rawValue)
+        case ThemeIdentifier.sunset.rawValue:
+            Freestyler.shared.changeTheme(to: ThemeIdentifier.sunrise.rawValue)
+        default:
+            break
+        }
+        
+        refreshChangeThemeButtonTitle()
+        UIView.animate(withDuration: 0.3) {
+            self.refreshStyles()
+        }
+    }
+    
+    private func refreshStyles() {
         primaryView.apply(style: PrimaryViewStyle())
         secondaryView.apply(style: SecondaryViewStyle())
         tertiaryView.apply(style: TertiaryViewStyle())
+    }
+    
+    private func refreshChangeThemeButtonTitle() {
+        let themeName: String
+        
+        switch Freestyler.shared.currentTheme?.identifier {
+        case ThemeIdentifier.sunrise.rawValue:
+            themeName = ThemeIdentifier.sunset.rawValue
+        case ThemeIdentifier.sunset.rawValue:
+            themeName = ThemeIdentifier.sunrise.rawValue
+        default:
+            themeName = ""
+            break
+        }
+
+        changeThemeButton.setTitle("Let's see the \(themeName)!", for: .normal)
     }
 }
 
